d75c050827520e2ab4ef61ab953e1a7a1ed67c66
TaeWan <139304010+Manwol770@users.noreply.github.com>
2023-08-18 00:27:32 +0900

    [Gold IV] Title: N-Queen, Time: 12396 ms, Memory: 138572 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,9 +1,8 @@
 import sys
 input = sys.stdin.readline
 
-def N_Queen(N, x_visited, y_visited) :           # 재귀 돌려줄 함수 (N은 x축을 담당할 것임)
+def N_Queen(N, y_visited) :           # 재귀 돌려줄 함수 (N은 x축을 담당할 것임)
     global cnt                      # cnt 에는 체스판 완성 횟수를 의미한다.
-    global cnt2
 
     if N == Num :                   # 재귀의 끝점까지 도달하면
                                     # = 체스판 끝까지 퀸을 놓았다면
@@ -32,11 +31,9 @@ def N_Queen(N, x_visited, y_visited) :           # 재귀 돌려줄 함수 (N은
                 else :
                     break
             if T_F :
-                x_visited[N] = 1
                 y_visited[i] = 1
                 matrix[N][i] = 1
-                N_Queen(N+1, x_visited, y_visited) # 재귀를 돌린다 (이 x축에 퀸을 놓았으니 x 축을 한칸더 가서 Y축을 다시 보겠다는 의미)
-                x_visited[N] = 0
+                N_Queen(N+1, y_visited) # 재귀를 돌린다 (이 x축에 퀸을 놓았으니 x 축을 한칸더 가서 Y축을 다시 보겠다는 의미)
                 y_visited[i] = 0
                 matrix[N][i] = 0
     return                              # for문을 다 돌았는데도 체스를 못놓았다면 다음으로 갈 필요없이 다시 위(x-1)로 돌아감
@@ -45,10 +42,8 @@ def N_Queen(N, x_visited, y_visited) :           # 재귀 돌려줄 함수 (N은
 Num = int(input())                # 받아오는 N
 
 cnt = 0
-cnt2 = 0
 y_visited = [0]*Num
-x_visited = [0]*Num
 matrix = [[0]*Num for _ in range (Num)]
 
-N_Queen(0, x_visited, y_visited)
+N_Queen(0, y_visited)
 print(cnt)
