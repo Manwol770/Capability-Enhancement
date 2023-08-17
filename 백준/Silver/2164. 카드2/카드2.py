4a2f923b174d5de8c71984793382eb027f13f91a
TaeWan <139304010+Manwol770@users.noreply.github.com>
2023-08-17 22:15:23 +0900

    [Silver IV] Title: 카드2, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,25 +1,8 @@
-import sys
-input = sys.stdin.readline
-
-num = int(input())
-two_num = 0
-n = num
+n,m = int(input()), 1
 
 while True :
+    if 1<<m >= 2 * n :
+        break
+    m += 1
 
-    if int(num/2) < 1 :
-        break 
-    num = num/2
-    two_num += 1
-
-
-if n == 1 :
-    m = 1
-
-elif n / 2**two_num == 1 :
-    m = n
-
-else :
-    m = 2 * (n - 2**two_num)
-
-print(m)
+print(2*n-2**(m-1))
