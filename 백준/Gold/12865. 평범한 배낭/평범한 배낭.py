dc89562a4642147bde4abbb42ca867c8d24433a9
TaeWan <139304010+Manwol770@users.noreply.github.com>
2023-09-04 17:23:18 +0900

    [Gold V] Title: 평범한 배낭, Time: 176 ms, Memory: 115272 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+dp = [0]*(K+1)
+
+for i in range(N):
+    w, v = map(int, input().split())
+    if K >= w:
+        for j in range(K, w-1, -1):
+            dp[j] = max(dp[j], dp[j-w] + v)
+
+print(max(dp))
