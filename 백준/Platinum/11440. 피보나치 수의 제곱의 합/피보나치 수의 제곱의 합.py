f608dd359bea4434f824250fb861dfc6a31862e8
TaeWan <139304010+Manwol770@users.noreply.github.com>
2023-09-19 15:53:53 +0900

    [Platinum V] Title: 피보나치 수의 제곱의 합, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Platinum/11440. 피보나치 수의 제곱의 합/피보나치 수의 제곱의 합.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def divide(lst1, lst2):
+    pb_f = [[(lst1[0][0] * lst2[0][0] + lst1[0][1] * lst2[1][0]) % 1000000007,
+             (lst1[0][0] * lst2[0][1] + lst1[0][1] * lst2[1][1]) % 1000000007],
+            [(lst1[1][0] * lst2[0][0] + lst1[1][1] * lst2[1][0]) % 1000000007,
+             (lst1[1][0] * lst2[0][1] + lst1[1][1] * lst2[1][1]) % 1000000007]]
+
+    return pb_f
+
+
+def pibo(num):
+    if num == 1:
+        return [[1, 1], [1, 0]]
+
+    pb_p = pibo(num // 2)
+    pb_f = divide(pb_p, pb_p)
+
+    if num % 2:
+        return divide(pb_f, [[1, 1], [1, 0]])
+    else:
+        return pb_f
+
+
+N = int(input())
+if N == 1:
+    print(1)
+else:
+    print(pibo(N)[1][0]*pibo(N+1)[1][0]%1000000007)
