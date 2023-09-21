e07f63a27163c8519eb095907ca36ca8ae817740
TaeWan <139304010+Manwol770@users.noreply.github.com>
2023-09-21 22:11:58 +0900

    [Gold IV] Title: 네트워크 연결, Time: 376 ms, Memory: 133828 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+import heapq
+
+V = int(input())
+E = int(input())
+nodes = [[] for _ in range(V)]
+visited = [0] * V
+
+for i in range(E):
+    a, b, c = map(int, input().split())
+    a -= 1
+    b -= 1
+    nodes[a].append((c, b))
+    nodes[b].append((c, a))
+
+heap = []
+heapq.heappush(heap, (0, 0))
+sum_w = 0
+
+while heap:
+
+    w, s = heapq.heappop(heap)
+
+    if visited[s]:
+        continue
+
+    visited[s] = 1
+
+    sum_w += w
+
+    for i in nodes[s]:
+        if visited[i[1]]:
+            continue
+        
+        heapq.heappush(heap, (i[0], i[1]))
+
+print(sum_w)
