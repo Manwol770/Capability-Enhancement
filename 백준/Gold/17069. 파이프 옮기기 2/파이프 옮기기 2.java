52a2f36a0bf3456df2c9d795f31dd2d50249ae17
TaeWan <139304010+Manwol770@users.noreply.github.com>
2024-04-04 13:29:33 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 136 ms, Memory: 14716 KB -BaekjoonHub

---
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class Main {
+	static int N;
+	static int [][] matrix;
+	static long [][][] visited;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		matrix = new int [N][];
+		visited = new long [N][N][3];
+		
+		for (int i = 0; i < N; i++) {
+			String [] cul_row = br.readLine().split(" ");
+			matrix[i] = Stream.of(cul_row).mapToInt(Integer::parseInt).toArray();
+			for (int j = 0; j < N; j++) {
+				Arrays.fill(visited[i][j], -1);
+			}
+		}
+		
+		System.out.println(dfs(0,1,0));
+	}
+	
+	public static long dfs(int x, int y, int dr) {
+		long tmp = 0;
+		
+		if (x == N - 1 && y == N -1) {
+			visited[x][y][dr] = 1;
+			return 1;
+		}
+		
+		if (visited[x][y][dr] != -1) {
+			return visited[x][y][dr];
+		}
+		
+		if ((dr == 0 || dr == 1) && y < N - 1 && matrix[x][y+1] != 1) {
+			tmp += dfs(x, y+1, 0);
+		}
+		
+		if (x < N - 1 && y < N - 1 && matrix[x][y+1] != 1 && matrix[x+1][y] != 1 && matrix[x+1][y+1] != 1) {
+			tmp += dfs(x+1, y+1, 1);
+		}
+		
+		if ((dr == 1 || dr == 2) && x < N - 1 && matrix[x+1][y] != 1) {
+			tmp += dfs(x+1, y, 2);
+		}
+		
+		visited[x][y][dr] = tmp;
+		return tmp;
+	}
+}
